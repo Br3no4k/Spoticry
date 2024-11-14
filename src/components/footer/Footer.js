eaaae94d3a11282c455abff75408ced574793ebb
br3no4k <brenoakuhn@gmail.com>
2024-11-13 22:55:04 -0300

    implementação do Footer na página

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/spoticrylogonav.png";
 
-const Footer = () => {
+export const Footer = () => {
   return (
-    <footer>
-      <p>Footer content here</p>
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <p>© 2024 Spoticry. Todos os direitos reservados. A música, sempre com você.</p>
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  );
-};
-
-export default Footer;
+  )
+}
