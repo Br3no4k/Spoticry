9d16c097f3021f8673958c80c19cf219c2cd60d1
br3no4k <brenoakuhn@gmail.com>
2024-12-02 13:07:27 -0300

    Testes com o footer da landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 //import Footer from "../components/LandingPage/Footer";
 import NavBar from '../components/LandingPage/NavBar';
 import Banner from '../components/LandingPage/Banner';
-import Footer from '../components/LandingPage/Footer';
+
 import { GlobalStyled } from "../GlobalStyled";
 
 export default function LandingPage() {
